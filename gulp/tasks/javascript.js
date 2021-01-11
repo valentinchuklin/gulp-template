4d00b4881a99c8b0c476ab18e52fb69a9365bbcf
Valentin Chuklin <valentinchuklin@gmail.com>
2021-01-11 13:12:42 +0300

    -

--- gulp/tasks/javascript.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const terser = require('gulp-terser');
- 
-module.exports = function es(){
-  return gulp.src(['./src/**/*.js', '!./src/**/includes/**/*'])
-    .pipe(terser())
-    .pipe(gulp.dest('./build/js/'));
-}
