4a40109270340a8e3d36843c5e2b94a96c9bd96b
Valentin Chuklin <valentinchuklin@gmail.com>
2021-03-01 19:40:28 +0300

    Задача перенесена в lab

--- gulp/tasks/makeSvgSymbolSprite.js
+++ gulp/tasks/makeSvgSymbolSprite.js
@@ -1,48 +1,14 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
+const gulpSvgSprite = require('gulp-svg-sprite');
+const htmlBeautify = require('gulp-html-beautify');
 
-//Сначала очищаем папку svg-sprite от старых спрайтов
-const gulpClean = require('gulp-clean')
-
-const cleanSpritesFunction = function () {
-  return gulp.src(['./src/common/svg-sprite/*'], { read: false })
-    .pipe(gulpClean())
-}
-//Очистим папку includes от спрайтов, чтобы случайно не скопировать в проект устаревший спрайт
-
-const cleanSpritesIncludesFunction = function () {
-  return gulp.src(['./src/common/includes/svg-sprite-shapes/symbol/svg/*'], { read: false })
-    .pipe(gulpClean())
-}
-
-//Затем создаёмм спрайт из файлов папки src/common/includes/svg-sprite-shapes
-const gulpSvgSprite = require('gulp-svg-sprite')
-
-//Зададим настройки спрайту
-const gulpSvgSpriteConfig = {
-  mode: {
-    inline: true, // Prepare for inline embedding
-    symbol: true, // Create a «symbol» sprite
-  }
-}
-//Сделаем спрайты хорошо читаемыми согласно стайлкоду
-const htmlBeautify = require('gulp-html-beautify')
-const htmlBeautifyOptions = {
-  "indent_size": 2
-}
-
-
+//Спрайт будет создан из svg-файлов в папке ./src/lab/svg-sprite/input/ и помещён в output
 const svgSymbolSpriteFunction = function () {
-  return gulp.src('./src/common/includes/svg-sprite-shapes/*.svg')
-    .pipe(gulpSvgSprite(gulpSvgSpriteConfig))
-    .pipe(htmlBeautify(htmlBeautifyOptions))
-    .pipe(gulp.dest('./src/common/includes/svg-sprite-shapes/'));
+  return gulp.src('./src/lab/svg-sprite/input/*.svg')
+    .pipe(gulpSvgSprite({mode: {inline: true, symbol: true}}))
+    .pipe(htmlBeautify({"indent_size": 2}))
+    .pipe(gulp.dest('./src/lab/svg-sprite/output/'));
 }
 
-//Копируем готовый спрайт в папку, из который gulp его подхватит для использования в проекте в build и dev-build, так как gulp игнорирует папку includes
-const copySvgSymbolSpriteForUseFunction = function () {
-  return gulp.src('./src/common/includes/svg-sprite-shapes/symbol/svg/*')
-    .pipe(gulp.dest('./src/common/svg-sprite/'));
-}
-
-const makeSvgSymbolSprite = gulp.series(cleanSpritesFunction, cleanSpritesIncludesFunction, svgSymbolSpriteFunction, copySvgSymbolSpriteForUseFunction, cleanSpritesIncludesFunction)
+const makeSvgSymbolSprite = gulp.series(svgSymbolSpriteFunction)
 module.exports = gulp.series(makeSvgSymbolSprite)
